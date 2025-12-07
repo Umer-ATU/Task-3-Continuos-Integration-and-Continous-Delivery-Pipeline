0b1c87226bd562313f8cbd9ad24b4ee48478f02e
Umer Karachiwala <karachiwalaumer2612@gmail.com>
2025-12-07 19:05:15 +0000

    updated response message of hello api

--- app/src/routes/hello.ts
+++ app/src/routes/hello.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const helloRouter = Router();
 
 helloRouter.get('/', (_, res) => {
-  res.type('text/plain').send('Hello from EKS!');
+  res.type('text/plain').send('Hello from EKS!!');
 });
 
 export default helloRouter;
